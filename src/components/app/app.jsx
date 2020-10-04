c47872b7495acf67f57c0721a2cf580af76be8f5
Ksenya TORKOVA <ksenyatorkova@o-s-i.org>
2020-10-04 20:37:06 +0300

    Добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -12,7 +12,37 @@ const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount = {errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <WelcomeScreen errorsCount = {errorsCount} />
+        </Route>
+        <Route path="/dev-artist" exact>
+          <Artist />
+        </Route>
+        <Route path="/dev-genre" exact>
+          <Genre />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/lose" exact>
+          <Losing />
+        </Route>
+        <Route path="/result" exact>
+          <Win />
+        </Route>
+        <Route>
+          <React.Fragment>
+            <h1>
+              404.
+              <br />
+              <small>Sorry, nothing found</small>
+            </h1>
+          </React.Fragment>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
