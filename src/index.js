be92901b5d3b788ccdd4f16ddd50e2cd60edc058
Ksenya TORKOVA <ksenyatorkova@o-s-i.org>
2020-10-18 21:42:06 +0300

    Подключает store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./store/reducer";
 import App from "./components/app/app";
 import questions from "./mocks/questions";
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Settings = {
   ERRORS_COUNT: 3,
 };
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+
 ReactDOM.render(
-    <App
-      errorsCount = {Settings.ERRORS_COUNT}
-      questions = {questions}
-    />,
+    <Provider store = {store}>
+      <App
+        errorsCount = {Settings.ERRORS_COUNT}
+        questions = {questions}
+      />,
+    </Provider>,
     document.querySelector(`#root`)
 );
